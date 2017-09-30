b8b958c0c04383c9ee6e600da2e5198e8d5a71b0
Jeffrey Liu <writetojeffrey@gmail.com>
2017-09-29 23:49:55 -0700

    Refactor: Extract RFCNTester (#2)

--- test.py
+++ test.py
@@ -28,78 +28,98 @@ from Visualize import Visualize
 from Utils import CheckpointLoader
 from Utils import PreviewIO
 
-parser = argparse.ArgumentParser(description="RFCN tester")
-parser.add_argument('-gpu', type=str, default="0", help='Train on this GPU(s)')
-parser.add_argument('-n', type=str, help='Network checkpoint file')
-parser.add_argument('-i', type=str, help='Input file.')
-parser.add_argument('-o', type=str, default="", help='Write output here.')
-parser.add_argument('-p', type=int, default=1, help='Show preview')
-parser.add_argument('-threshold', type=float, default=0.5, help='Detection threshold')
-parser.add_argument('-delay', type=int, default=-1, help='Delay between frames in visualization. -1 for automatic, 0 for wait for keypress.')
-parser.add_argument('-dataset', type=str, default="/home/eljefec/data/nexet/train", help="Path to Nexet dataset")
-parser.add_argument('-annotation', type=str, default="/home/eljefec/data/nexet/train_boxes.simple.csv", help="Path to annotation csv")
+def main():
+    parser = argparse.ArgumentParser(description="RFCN tester")
+    parser.add_argument('-gpu', type=str, default="0", help='Train on this GPU(s)')
+    parser.add_argument('-n', type=str, help='Network checkpoint file')
+    parser.add_argument('-i', type=str, help='Input file.')
+    parser.add_argument('-o', type=str, default="", help='Write output here.')
+    parser.add_argument('-p', type=int, default=1, help='Show preview')
+    parser.add_argument('-threshold', type=float, default=0.5, help='Detection threshold')
+    parser.add_argument('-delay', type=int, default=-1, help='Delay between frames in visualization. -1 for automatic, 0 for wait for keypress.')
+    parser.add_argument('-dataset', type=str, default="/home/eljefec/data/nexet/train", help="Path to Nexet dataset")
+    parser.add_argument('-annotation', type=str, default="/home/eljefec/data/nexet/train_boxes.simple.csv", help="Path to annotation csv")
 
-opt=parser.parse_args()
-os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu
+    opt=parser.parse_args()
+    os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpu
 
-dataset = BoxLoader()
-dataset.add(NexetDataset(opt.dataset, opt.annotation))
-print("Number of categories: "+str(dataset.categoryCount()))
-print(dataset.getCaptionMap())
-categories = dataset.getCaptionMap()
-
-palette = Visualize.Palette(dataset.categoryCount())
-
-image = tf.placeholder(tf.float32, [None, None, None, 3])
-net = BoxInceptionResnet(image, dataset.categoryCount(), name="boxnet")
-
-boxes, scores, classes = net.getBoxes(scoreThreshold=opt.threshold)
-
-
-input = PreviewIO.PreviewInput(opt.i)
-output = PreviewIO.PreviewOutput(opt.o, input.getFps())
+    test_rfcn(opt)
 
 def preprocessInput(img):
-	def calcPad(size):
-		m = size % 32
-		p = int(m/2)
-		s = size - m
-		return s,p
+    def calcPad(size):
+        m = size % 32
+        p = int(m/2)
+        s = size - m
+        return s,p
 
-	zoom = max(640.0 / img.shape[0], 640.0 / img.shape[1])
-	img = cv2.resize(img, (int(zoom*img.shape[1]), int(zoom*img.shape[0])))
+    zoom = max(640.0 / img.shape[0], 640.0 / img.shape[1])
+    img = cv2.resize(img, (int(zoom*img.shape[1]), int(zoom*img.shape[0])))
 
-	if img.shape[0] % 32 != 0:
-		s,p = calcPad(img.shape[0])
-		img = img[p:p+s]
+    if img.shape[0] % 32 != 0:
+        s,p = calcPad(img.shape[0])
+        img = img[p:p+s]
 
-	if img.shape[1] % 32 != 0:
-		s,p = calcPad(img.shape[1])
-		img = img[:,p:p+s]
+    if img.shape[1] % 32 != 0:
+        s,p = calcPad(img.shape[1])
+        img = img[:,p:p+s]
 
-	return img
+    return img
 
-with tf.Session() as sess:
-	if not CheckpointLoader.loadCheckpoint(sess, 'save/save', opt.n, ignoreVarsInFileNotInSess=True):
-		print("Failed to load network.")
-		sys.exit(-1)
+class RFCNTester:
+    def __init__(self, save_dir, network_file, bbox_threshold,
+                 dataset = '/home/eljefec/data/nexet/train',
+                 annotation = '/home/eljefec/data/nexet/train_boxes.simple.csv'):
+        dataset = BoxLoader()
+        dataset.add(NexetDataset(dataset, annotation))
+        print("Number of categories: "+str(dataset.categoryCount()))
+        print(dataset.getCaptionMap())
+        self.categories = dataset.getCaptionMap()
 
-	while True:
-		img = input.get()
-		if img is None:
-			break
+        self.image = tf.placeholder(tf.float32, [None, None, None, 3])
+        self.net = BoxInceptionResnet(self.image, dataset.categoryCount(), name="boxnet")
 
-		img = preprocessInput(img)
+        self.boxes, self.scores, self.classes = self.net.getBoxes(scoreThreshold=bbox_threshold)
 
-		rBoxes, rScores, rClasses = sess.run([boxes, scores, classes], feed_dict={image: np.expand_dims(img, 0)})
+        self.sess = tf.Session()
 
-		res = Visualize.drawBoxes(img, rBoxes, rClasses, [categories[i] for i in rClasses.tolist()], palette, scores=rScores)
+        if not CheckpointLoader.loadCheckpoint(self.sess, save_dir, network_file, ignoreVarsInFileNotInSess=True):
+            print("Failed to load network.")
+            sys.exit(-1)
 
-		output.put(input.getName(), res)
+    def predict(self, img):
+        img = preprocessInput(img)
 
-		if opt.p==1:
-			cv2.imshow("result", res)
-			if opt.o=="":
-				cv2.waitKey(input.getDelay() if opt.delay <0 else opt.delay)
-			else:
-				cv2.waitKey(1)
+        rBoxes, rScores, rClasses = self.sess.run([self.boxes, self.scores, self.classes], feed_dict={self.image: np.expand_dims(img, 0)})
+
+        return rBoxes, rScores, rClasses
+
+def test_rfcn(opt):
+    model = RFCNTester('save/save', opt.n, opt.threshold, opt.dataset, opt.annotation)
+
+    input = PreviewIO.PreviewInput(opt.i)
+    output = PreviewIO.PreviewOutput(opt.o, input.getFps())
+
+    palette = Visualize.Palette(len(model.categories))
+
+    while True:
+        img = input.get()
+        if img is None:
+            break
+
+        img = preprocessInput(img)
+
+        rBoxes, rScores, rClasses = model.predict(img)
+
+        res = Visualize.drawBoxes(img, rBoxes, rClasses, [model.categories[i] for i in rClasses.tolist()], palette, scores=rScores)
+
+        output.put(input.getName(), res)
+
+        if opt.p==1:
+            cv2.imshow("result", res)
+            if opt.o=="":
+                cv2.waitKey(input.getDelay() if opt.delay <0 else opt.delay)
+            else:
+                cv2.waitKey(1)
+
+if __name__ == '__main__':
+    main()
